57a730da396f8b33511237ffbf75f6e343b6cdfc
Manasvi Choudhary <manasvichoudhary180@gmail.com>
2026-03-15 14:43:12 +0530

    fix: MongoDB connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-export const connectDB=async()=>{
-    await mongoose.connect('mongodb+srv://manasvichoudhary438:resume123@cluster0.qgjli7l.mongodb.net/RESUME')
-    .then(()=>console.log('DB CONNECTED'))
+
+export const connectDB = async () => {
+    await mongoose.connect('mongodb+srv://manasvichoudhary438:resume20@cluster0.qgjli7l.mongodb.net/RESUME')
+        .then(() => console.log('DB CONNECTED'))
 }
