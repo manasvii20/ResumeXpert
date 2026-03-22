b827e9080010cefebd13048e7d4a0ecdb1f50900
Manasvi Choudhary <manasvichoudhary180@gmail.com>
2026-03-22 22:22:47 +0530

    route fixes

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -2,25 +2,25 @@ import axios from 'axios'
 import { BASE_URL } from '../utils/apiPaths'
 // import { response } from 'express'
 
-const axiosInstance=axios.create({
-    baseURL:BASE_URL,
-    timeout:10000,
-    headers:{
-        "Content-Type":"application/json",
-        Accept:"application/json",
+const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    timeout: 10000,
+    headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
     }
 })
 
 //REQUEST INTERCEPTER
 axiosInstance.interceptors.request.use(
-    (config)=>{
-        const accessToken=localStorage.getItem('token')
-        if(accessToken){
-            config.headers.Authorization=`Bearer ${accessToken}`
+    (config) => {
+        const accessToken = localStorage.getItem('token')
+        if (accessToken) {
+            config.headers.Authorization = `Bearer ${accessToken}`
         }
         return config;
     },
-    (error)=>{
+    (error) => {
         return Promise.reject(error)
     }
 )
@@ -28,19 +28,21 @@ axiosInstance.interceptors.request.use(
 //RESPONSE INTERCEPTER
 
 axiosInstance.interceptors.response.use(
-    (response)=>{
+    (response) => {
         return response;
     },
-    (error)=>{
-        if(error.response){
-            if(error.response.status===401){
-                window.location.href='/'
+    (error) => {
+        if (error.response) {
+            // Redirect to home if 401 (Unauthorized) occurs, EXCEPT for login/register requests
+            const isAuthRequest = error.config.url.includes('/api/auth/login') || error.config.url.includes('/api/auth/register');
+            if (error.response.status === 401 && !isAuthRequest) {
+                window.location.href = '/'
             }
-            else if(error.response.status===500){
+            else if (error.response.status === 500) {
                 console.error("Server Error")
             }
         }
-        else if(error.code==='ECONNABORTED'){
+        else if (error.code === 'ECONNABORTED') {
             console.error("Request timeout")
         }
         return Promise.reject(error)
